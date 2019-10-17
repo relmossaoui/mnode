72c23db09cb067ae829febdbb0bd35894bf45fdc
relmossaoui <redouan.elmossaoui2@gmail.com>
2019-10-17 01:42:53 +0100

    feat : add middleware function && global configs.

--- test/app.js
+++ test/app.js
@@ -3,13 +3,16 @@ const MNode = require('../lib/mnode')
 
 const app = new MNode()
 
+app.set(MNode.configs.STATIC_PATH, path.resolve(path.dirname('')) + '/views')
+
+app.middleware(MNode.parseUrl);
+
+app.middleware(MNode.staticServer);
 
 app.get('/test', (req, res) => {
-    res.end('test')
+    res.end(JSON.stringify(req.pathname))
 });
 
-app.post('/', (req, res) => {res.end('data has been received correctly')})
-
-console.log(app.stack)
+app.post('/', (req, res) => {res.end('data has been posted correctly ')})
 
 app.start(8081, () => { console.log('server at 8081')})
